29253dff2fb5bc072d4eb562051b8b7b0b621734
puneetsahota <puneetsinghsahota@gmail.com>
2025-11-11 04:38:29 +0000

    Adding basic spring application with spring web and maven build properties.

---
+++ src/main/java/com/medexpress/genoviaconsultationapi/GenoviaConsultationApiApplication.java
@@ -0,0 +1,11 @@
+package com.medexpress.genoviaconsultationapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GenoviaConsultationApiApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(GenoviaConsultationApiApplication.class, args);
+    }
+}
